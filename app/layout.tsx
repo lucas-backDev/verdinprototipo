025ec5fc53123967c8bf6804753bb4b7d27cf151
lucas-backDev <lucas.cgomesb19@gmail.com>
2025-04-14 10:38:44 -0300

    feat: criando biblioteca de botões

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const fonte1 = Nunito({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const fonte2 = Nunito({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fonte1.variable} ${fonte2.variable} antialiased`}
       >
         {children}
       </body>
